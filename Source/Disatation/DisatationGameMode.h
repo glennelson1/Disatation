7d665bff83051da379a4f605e6ee1debcb8540e9
Glennelson1 <70724924+Glennelson1@users.noreply.github.com>
2024-06-23 21:23:18 +0100

    Initial commit

---
+++ Source/Disatation/DisatationGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DisatationGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADisatationGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADisatationGameMode();
+};
+
+
+
